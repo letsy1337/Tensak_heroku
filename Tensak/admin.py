db9ef22b70fce1c492105b1a9b188085f5de6c71
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 01:55:35 +0300

    Made Procreator class

---
+++ Tensak/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from main_Tensak.models import Procreator
+admin.site.register(Procreator)
--- Tensak/settings.py
+++ Tensak/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_Tensak'
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
